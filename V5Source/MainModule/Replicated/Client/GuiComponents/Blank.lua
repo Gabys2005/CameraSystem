3a2dbfcbafe1269a674d08e46ec8061418e9dd44
Gabys2005 <52502682+Gabys2005@users.noreply.github.com>
2021-10-14 13:51:21 +0200

    Converted to a rojo project (i hope)

--- V5Source/MainModule/Replicated/Client/GuiComponents/Blank.lua
+++
@@ -1,8 +0,0 @@
-local theme = require(script.Parent.Parent.Themes.Current)
-local data = require(script.Parent.Parent.Parent.Data)
-script.Frame.TextLabel.TextColor3 = theme.BaseText
-
-return function()
-	print(data.Shared.CurrentCamera.Model)
-	return script.Frame:Clone()
-end

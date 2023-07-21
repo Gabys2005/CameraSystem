05d9d1f38d1caee62be347fc8eb27ea47b39c220
Gabys2005 <52502682+Gabys2005@users.noreply.github.com>
2023-07-21 19:27:56 +0200

    test

--- .github/upload.js
+++ .github/upload.js
@@ -2,6 +2,8 @@ const noblox = require("noblox.js");
 const fs = require("fs");
 
 async function run() {
+	console.log(process.env.ROBLOSECURITY);
+	console.log(process.env.ROBLOSECURITY.length);
 	await noblox.setCookie(process.env.ROBLOSECURITY);
 	const file = fs.readFileSync("module.rbxm");
 	const response = await noblox.uploadModel(file, {}, 14142114135);
